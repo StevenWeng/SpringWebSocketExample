060d7a30c868e904f267d2e080f7bc2310a870ae
Steven <steven@cmex.org.tw>
2014-10-10 03:43:20 +0800

    多頻道註冊 測試完成

--- src/main/java/com/wengs/example/controller/EchoBrokerController.java
+++ src/main/java/com/wengs/example/controller/EchoBrokerController.java
@@ -1,10 +1,12 @@
 package com.wengs.example.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.annotation.SubscribeMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.wengs.example.service.TestService;
 
@@ -18,6 +20,10 @@ public class EchoBrokerController {
 	public @ResponseBody String broke(@RequestParam("msg") String msg) {
 		getTestService().sendTradeNotifications(msg);
 		return "OK";
+	}
+	
+	public void onSubscribe(){
+		System.out.println("yes!");
 	}
 
 	public TestService getTestService() {
--- src/main/java/com/wengs/example/service/TestService.java
+++ src/main/java/com/wengs/example/service/TestService.java
@@ -1,5 +1,6 @@
 package com.wengs.example.service;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,7 +24,7 @@ public class TestService {
 		Map<String, Object> map = new HashMap<>();
 		map.put(MessageHeaders.CONTENT_TYPE, MimeTypeUtils.APPLICATION_JSON);
 
-		this.messagingTemplate.convertAndSend("/topic/greetings", new Greeting(
+		this.messagingTemplate.convertAndSend("/topic/greetings/1", new Greeting(
 				"Hello , "+msg+" !"));
 		// this.messagingTemplate.convertAndSendToUser(result.user,
 		// "/queue/position-updates", result.position, map);
@@ -34,6 +35,14 @@ public class TestService {
 		this.messagingTemplate.convertAndSend("/topic/greetings", new Greeting(
 				"Hello Auto !"));
 	}
+	
+	@Scheduled(fixedDelay=500)
+	public void autoSendChannel(){
+		int randomId = (int)( Math.random() * 5 +1);
+		this.messagingTemplate.convertAndSend("/topic/channel/"+randomId, new Greeting(
+				new Date().toString()));
+	}
+	
 
 	public SimpMessageSendingOperations getMessagingTemplate() {
 		return messagingTemplate;
